ae92f59b0024b066b62ebb3a7ff721b3d98d1e6d
randompast <matteroflight@gmail.com>
2022-10-01 18:13:41 -0400

    added richochet bullets that bounce off of a collision normal

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -10,8 +10,15 @@ func shoot() -> void:
 		#print("shooting projectile")
 		var inst: Projectile = projectile.instantiate()
 		owner.add_child(inst)
-		inst.transform = self.transform
+		
+		#set to spawner location
+		inst.position = self.position
+
+		#look_at direction
 		var look = get_viewport().get_mouse_position()
 		inst.look_at(look)
+		var angle = inst.transform.get_rotation()
+		inst.direction = Vector2(cos(angle), sin(angle))
+
 		#create an offset so that it doesn't fire at the player's position
 		inst.position += (look - self.position).normalized()*20
