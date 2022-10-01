d923dd6339e9d167d661f1a38fed5636a3d2914c
randompast <matteroflight@gmail.com>
2022-10-01 16:30:28 -0400

    got projectiles colliding

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -5,6 +5,5 @@ class_name Projectile
 var speed: float = 2.0
 
 func _physics_process(_delta: float) -> void:
-	position += transform.x * speed
-#	move_and_collide(Vector2.RIGHT, speed)
-
+#	position += transform.x * speed
+	move_and_collide(transform.x * speed)
