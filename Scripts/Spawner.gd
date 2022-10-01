107ef6a048042765a54ebe027776f6a59aada8e4
randompast <matteroflight@gmail.com>
2022-10-01 18:25:33 -0400

    increased window resolution, showed spawning enemy on click in spawnertest scene, should have something else trigger it

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -1,9 +1,23 @@
 extends CharacterBody2D
 
 @onready var projectile = preload("res://Scenes/Projectile.tscn")
+@onready var chaserenemy = preload("res://Scenes/ChaserEnemy.tscn")
+
+var spawn_count: int = 5
 
 func _physics_process(_delta: float) -> void:
-	shoot()
+#	shoot()
+	spawn()
+
+func spawn() -> void:
+	if Input.is_action_just_pressed("shoot") and spawn_count > 0:
+		spawn_count -= 1
+		#print("shooting projectile")
+		var inst: Enemy = chaserenemy.instantiate()
+		owner.add_child(inst)
+		
+		#set to spawner location
+		inst.position = self.position
 
 func shoot() -> void:
 	if Input.is_action_just_pressed("shoot"):
