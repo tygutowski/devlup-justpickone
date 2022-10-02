ae881828ed1ccff20bd0c1acd63d3d61144b8892
JophASL <joogasl@gmail.com>
2022-10-01 20:16:41 -0400

    Fixed bug where UI would not show up back again when pressing Resume button

--- ui/hud.gd
+++ ui/hud.gd
@@ -3,6 +3,5 @@ extends CanvasLayer
 func _ready():
 	visible = true
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("escape"):
-		visible = !visible
+func _process(_delta: float) -> void:
+	visible = !get_tree().paused
