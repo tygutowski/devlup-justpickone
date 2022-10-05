fdfee549fe97637c122fed826e2d58a53918a9f7
JophASL <joogasl@gmail.com>
2022-10-04 22:13:04 -0400

    Optimizing (?) player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -84,52 +84,58 @@ func _ready():
 	$BossMusic.playing = false
 
 func _physics_process(_delta):
-	if !in_menu:
-		if currently_reloading:
-			if(reload_time > reload_timer):
-				currently_reloading = false
-				reload_time = 0
-				ammo = ammo_max
-			reload_time += 1
+	if in_menu:
+		return
+	
+	if currently_reloading:
+		if reload_time > reload_timer:
+			currently_reloading = false
+			reload_time = 0
+			ammo = ammo_max
+		reload_time += 1
+	else:
+		walking_direction = Vector2(
+			Input.get_axis("left", "right"),
+			Input.get_axis("up", "down"),
+		).normalized()
+		
+		velocity = walking_direction * speed
+		move_and_slide()
+		
+		if walking_direction != Vector2.ZERO:
+			facing_direction = walking_direction
+		
+		if walking_direction == Vector2.ZERO:
+			set_animation("idle_or_walking", 0)
 		else:
-			walking_direction = Vector2.ZERO
-			walking_direction.x = Input.get_axis("left", "right")
-			walking_direction.y = Input.get_axis("up", "down")
-			walking_direction = walking_direction.normalized()
-			velocity = walking_direction * speed
-			move_and_slide()
-			if walking_direction != Vector2.ZERO:
-				facing_direction = walking_direction
-			if walking_direction == Vector2.ZERO:
-				set_animation("idle_or_walking", 0)
+			set_animation("idle_or_walking", 1)
+			if facing_direction.x < 0: # Left
+				set_animation("direction", 2)
+			elif facing_direction.x > 0: # Right
+				set_animation("direction", 3)
+			elif facing_direction.y > 0: # Down
+				set_animation("direction", 1)
+			elif facing_direction.y < 0: # Up
+				set_animation("direction", 0)
+		
+		if Input.is_action_just_pressed("shoot") && shot_time >= shot_timer:
+			if ammo > 0:
+				shoot()
+				shot_time = 0
+				ammo -= 1
 			else:
-				set_animation("idle_or_walking", 1)
-				if facing_direction.x < 0: # Left
-					set_animation("direction", 2)
-				elif facing_direction.x > 0: # Right
-					set_animation("direction", 3)
-				elif facing_direction.y > 0: # Down
-					set_animation("direction", 1)
-				elif facing_direction.y < 0: # Up
-					set_animation("direction", 0)
-			
-			
-			if Input.is_action_just_pressed("shoot") && shot_time >= shot_timer:
-				if ammo > 0:
-					shoot()
-					shot_time = 0
-					ammo -= 1
-				else:
-					$HUD.show_reload_tooltip()
-					$FX/GunJammedFX.play()
-			if Input.is_action_just_pressed("reload") && ammo != ammo_max:
-				$FX/ReloadFX.play()
-				currently_reloading = true
-			
-			if Input.is_action_just_pressed("new_map"):
-				LevelGenerator.level += 2
-				get_tree().change_scene_to_file("res://Scenes/Game.tscn")
-			shot_time += 1
+				$HUD.show_reload_tooltip()
+				$FX/GunJammedFX.play()
+		
+		if Input.is_action_just_pressed("reload") && ammo != ammo_max:
+			$FX/ReloadFX.play()
+			currently_reloading = true
+		
+		if Input.is_action_just_pressed("new_map"):
+			LevelGenerator.level += 2
+			get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+		
+		shot_time += 1
 
 func set_animation(animation_name: String, value: int):
 	if animation_name == "direction":
@@ -138,14 +144,13 @@ func set_animation(animation_name: String, value: int):
 	else:
 		get_node("AnimationTree").set("parameters/" + animation_name + "/current", value)
 
-func shoot():
+func shoot() -> void:
 	# make a new shot
-	var pos := global_position
 	for x in range(shot_count):
-		var hit_list := []
 		var discrepancy := Vector2.ZERO
 		if x != 0:
 			discrepancy = Vector2(randi_range(0,25), randi_range(0,25))
+		var hit_list := []
 		for i in range(pierce_count):
 			var ray : Node2D = shot.instantiate()
 			var raycast : RayCast2D = ray.get_node("RayCast2d")
@@ -153,16 +158,16 @@ func shoot():
 				raycast.add_exception(object)
 			game.add_child(ray)
 			# set position to player
-			ray.get_node("RayCast2d").global_position = global_position
+			raycast.global_position = global_position
 			# set target to mouse
-
 			raycast.target_position = (get_local_mouse_position() + discrepancy).normalized() * 100000
 			raycast.enabled = true
 			raycast.force_raycast_update()
 			ray.get_node("Line2d").add_point(raycast.global_position)
-			pos = raycast.get_collision_point()
+			var pos := raycast.get_collision_point()
 			if pos == Vector2.ZERO:
 				pos = raycast.target_position
+			
 			ray.get_node("Line2d").add_point(pos)
 			$FX/ShootFX.play()
 			
@@ -175,14 +180,15 @@ func shoot():
 			for u in LevelGenerator.upgrades:
 				if u.explosive_shot:
 					var expl : Node2D = load("res://Scenes/Explosion.tscn").instantiate()
-					expl.global_position = ray.get_node("RayCast2d").get_collision_point()
+					expl.global_position = raycast.get_collision_point()
 					game.add_child(expl)
-func upgrade():
+
+func upgrade() -> void:
 	in_menu = true
 	var upgrade_menu : CanvasLayer = load("res://Scenes/UpgradeScreen.tscn").instantiate()
 	add_child(upgrade_menu)
 
-func exit_menu():
+func exit_menu() -> void:
 	in_menu = false
 	shot_time = 0
 
@@ -195,6 +201,7 @@ func takeDamage(damage_amount : int) -> void:
 	if health <= 0:
 		health = 0
 		$AnimationPlayer.play("died")
+		await $AnimationPlayer.animation_finished
 		get_tree().reload_current_scene()
 		return
 	
@@ -202,9 +209,3 @@ func takeDamage(damage_amount : int) -> void:
 	$AnimationPlayer.play("hurt")
 	await $AnimationPlayer.animation_finished
 	vulnerable = true
-
-func _on_hurtbox_body_entered(body):
-	if body is Enemy == false:
-		return
-	
-	$FX/HurtFX.play()
